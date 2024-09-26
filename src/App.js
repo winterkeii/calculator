040df2cee7b4829092f03ed710e3388b0a91c6b9
winterkeii <johnleomedina@gmail.com>
2024-09-26 11:47:02 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+function Button({label, clickHandler}){
+  return(
+    <button onClick={clickHandler}>
+      {label}
+    </button>
+    
+  );
+}
+
+function Display(){
+  return(
+    <div className="Display">
+      0
+    </div>
+  );
+}
+
+
 function App() {
+  const genenricclickHandler = (e) => {
+    e.preventDefault();
+    const value  = e.target.innerHTML;
+    console.log(value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculator">
+      <div className="Calcontainer">
+        <div className="Displaycon">
+        <Display></Display>
+        </div>
+        <div className="Buttons">
+          <Button label={7} clickHandler={genenricclickHandler}/> 
+          <Button label={8} clickHandler={genenricclickHandler}/>
+          <Button label={9} clickHandler={genenricclickHandler}/>
+          <Button label={'/'} clickHandler={genenricclickHandler}/>
+          <Button label={4} clickHandler={genenricclickHandler}/>
+          <Button label={5} clickHandler={genenricclickHandler}/>
+          <Button label={6} clickHandler={genenricclickHandler}/>
+          <Button label={'*'} clickHandler={genenricclickHandler}/>
+          <Button label={1} clickHandler={genenricclickHandler}/>
+          <Button label={2} clickHandler={genenricclickHandler}/>
+          <Button label={3} clickHandler={genenricclickHandler}/>
+          <Button label={'-'} clickHandler={genenricclickHandler}/>
+          <Button label={'.'} clickHandler={genenricclickHandler}/>
+          <Button label={0} clickHandler={genenricclickHandler}/>
+          <Button label={'='} clickHandler={genenricclickHandler}/>
+          <Button label={'+'} clickHandler={genenricclickHandler}/>
+        </div>
+      </div>
     </div>
   );
 }
